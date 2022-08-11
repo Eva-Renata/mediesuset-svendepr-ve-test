ae5cec67fccf7b0f3475d13b1f2d4a5c7f913469
Eva-Renata <all2evi@yahoo.com>
2022-08-11 12:26:44 +0200

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world, mediesuset</h1>
+    </div>
+  );
+}
+
+export default App;
